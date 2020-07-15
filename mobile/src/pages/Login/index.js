291673ed2112404d38a5aea9214c5b254c201836
Arthur <arthurarp@hotmail.com>
2020-07-15 19:25:50 -0300

    criando tela login com fundo branco

--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -1,10 +1,156 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect }  from 'react';
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  Animated,
+  Keyboard,
+  StatusBar,
+  Image,
+  Dimensions
+} from 'react-native';
+
+import styles from './styles';
 
 export default function Login() {
-    return(
-        <View>
-           <Text> Login Page </Text>
-       </View>
-    );
-}
+
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+  const [offset] = useState(new Animated.ValueXY({x:0, y:80}));
+  const [opacity] = useState(new Animated.Value(0));
+  const [logo] = useState(new Animated.ValueXY({x: 300, y: 300}));
+
+  useEffect(() => {
+
+    keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow);
+    keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHide);
+
+    Animated.parallel([
+      Animated.spring(offset.y, {
+        toValue: 0,
+        speed: 4,
+        bounciness: 25,
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 4000,
+      }),
+    ]).start();
+    
+  }, []);
+
+  function keyboardDidShow() {
+    
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 200,
+        duration: 200,
+      }),
+      Animated.timing(logo.y, {
+        toValue: 200,
+        duration: 200,
+      }),
+    ]).start();
+    
+  }
+
+  function keyboardDidHide() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 260,
+        duration: 200,
+      }),
+      Animated.timing(logo.y, {
+        toValue: 260,
+        duration: 200,
+      }),
+    ]).start(); 
+  }
+
+  console.disableYellowBox = true
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+      <StatusBar barStyle='dark-content' backgroundColor="#FFF" />
+      <View style={styles.containerLogo}>
+        <Animated.Image
+          style={{ width: logo.x, height: logo.y}}
+          source={require('../../assets/logo3.jpg')}
+        />
+
+
+      </View>
+
+      <Animated.View 
+        style={[
+          styles.inputContainer,
+          {
+            opacity: opacity,
+            transform: [
+              { translateY: offset.y }
+            ]
+          }
+        ]}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={() => {}}
+          secureTextEntry={true}
+        />
+
+        <TouchableOpacity
+            style={styles.btnSubmit}
+            onPress={() => alert(windowWidth + ' h: ' + windowHeight)}    
+        >
+          <Text style={styles.submitText}>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.btnRegister}
+          onPress={() => {}}
+        >
+            <Text style={styles.RegisterText}>Criar conta</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.btnRegister}
+          onPress={() => {}}
+        >
+        </TouchableOpacity>
+        
+        <View style={{flex: 1, width: '60%', flexDirection: 'column', justifyContent: 'space-evenly'}}>
+          <View style={{flexDirection: 'row',marginBottom: 10}}>
+            <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center'}}>
+              {/* <Image style={{width: 20, height: 20}} source={require('../../assets/logo_facebook.png')}/> */}
+              <Text style={{color: '#FFF',}}>Conecte-se com o Facebook</Text>
+            </TouchableOpacity>
+
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center'}}>
+              {/* <Image style={{width: 20, height: 20}} source={require('../../assets/logo_google.jpg')}/> */}
+              <Text style={{color: '#FFF',}}>Conecte-se com o Google</Text>
+            </TouchableOpacity>
+
+          </View>
+
+        </View>
+        
+            
+
+      </Animated.View>
+
+      
+    </KeyboardAvoidingView>
+  );
+};
--- mobile/src/pages/Login/styles.js
+++ mobile/src/pages/Login/styles.js
@@ -1,3 +1,52 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+export default StyleSheet.create({
+    background: {
+      flex: 1,
+      // backgroundColor: '#000',
+    },
+    containerLogo: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputContainer: {
+      flex: 1,
+      alignItems: 'center',
+      // backgroundColor: '#000',
+    },
+    input: {
+      // backgroundColor: 'black',
+      width: '90%',
+      marginBottom: 15,
+      color: '#242940',
+      fontSize: 17,
+      borderRadius: 7,
+      padding: 12,
+      borderWidth: 2,
+      borderColor: '#36b395'      
+    },
+    btnSubmit: {
+      backgroundColor: '#36b395',//'#00c4cc','#242940',
+      width: '40%',
+      height: 45,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 7,
+      padding: 20,
+      marginTop: 5,
+    },
+    submitText: {
+      color: '#FFF',
+      fontSize: 18,
+      //fontWeight: 'bold'
+    },
+    btnRegister: {
+      marginTop: 10,
+    },
+    RegisterText: {
+      color: '#36b395',
+      padding: 10,
+      fontWeight: 'bold',
+    },
+  });
