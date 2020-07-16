3eef0e765d77dad4460077a27e1aa9deab59ccfb
Arthur <arthurarp@hotmail.com>
2020-07-16 20:12:07 -0300

    criando arquivos para tela de cadastro do cachuapp, adicionando navegação em pilha no arquivo routes.js

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Login from './pages/Login';
+import Register from './pages/Register/' 
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function Routes() {
+
+    const AppStack = createStackNavigator();
+    
     return (
-       <Login />
+        <NavigationContainer>
+            <AppStack.Navigator>
+                <AppStack.Screen
+                    name="Login"
+                    component={Login}
+                    options={{
+                        headerShown: false
+                    }}
+                    
+        
+                />
+
+            <AppStack.Screen
+                    name="Register"
+                    component={Register}
+                    options={{
+                        headerShown: false
+                    }}
+                    
+        
+                />  
+            </AppStack.Navigator>
+
+        </NavigationContainer>
     );
 }
