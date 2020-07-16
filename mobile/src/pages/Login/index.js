910ca64a64ffaf137f9102b4b68603523c27f32d
Arthur <arthurarp@hotmail.com>
2020-07-16 19:45:35 -0300

    algumas mudanças

--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -16,23 +16,30 @@ import styles from './styles';
 
 export default function Login() {
 
+//   Pegando a largura e altura da tela do usuário
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
+// Variável 'offset em 80 apenas no eixo y, pois a animação so ocorre no sentido vertical' 
   const [offset] = useState(new Animated.ValueXY({x:0, y:80}));
+// Variável 'opacity' 0, esse valor é atualizado para 1 durante da animação
   const [opacity] = useState(new Animated.Value(0));
+// Variável 'logo' guarda o valores inicias das dimensões da logomarca
   const [logo] = useState(new Animated.ValueXY({x: 300, y: 300}));
 
   useEffect(() => {
 
+// Funções para detectar se o teclado está aberto e decidir o que fazer com isso
     keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow);
     keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHide);
 
+//
     Animated.parallel([
       Animated.spring(offset.y, {
         toValue: 0,
-        speed: 4,
+        speed: 1,
         bounciness: 25,
       }),
+// Animaçao para a alterar a opacidade, que inicia com zero, durante o prazo de 4000ms o valor vai subindo até chegar a 1.
       Animated.timing(opacity, {
         toValue: 1,
         duration: 4000,
@@ -40,31 +47,31 @@ export default function Login() {
     ]).start();
     
   }, []);
-
+// Quando o teclado estiver ativo, essa função diminui o tamanho da logo
   function keyboardDidShow() {
     
     Animated.parallel([
       Animated.timing(logo.x, {
         toValue: 200,
-        duration: 200,
+        duration: 500,
       }),
       Animated.timing(logo.y, {
         toValue: 200,
-        duration: 200,
+        duration: 500,
       }),
     ]).start();
     
   }
-
+// Quando o teclado estiver inativo, essa função retorna o tamanho original da logo
   function keyboardDidHide() {
     Animated.parallel([
       Animated.timing(logo.x, {
-        toValue: 260,
-        duration: 200,
+        toValue: 300,
+        duration: 500,
       }),
       Animated.timing(logo.y, {
-        toValue: 260,
-        duration: 200,
+        toValue: 300,
+        duration: 500,
       }),
     ]).start(); 
   }
@@ -78,7 +85,6 @@ export default function Login() {
           style={{ width: logo.x, height: logo.y}}
           source={require('../../assets/logo3.jpg')}
         />
-
 
       </View>
 
@@ -98,6 +104,7 @@ export default function Login() {
           placeholder="E-mail"
           autoCorrect={false}
           onChangeText={() => {}}
+          keyboardType='email-address'
         />
 
         <TextInput
--- mobile/src/pages/Login/styles.js
+++ mobile/src/pages/Login/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     background: {
       flex: 1,
-      // backgroundColor: '#000',
+      backgroundColor: '#FFF',
     },
     containerLogo: {
         flex: 1,
